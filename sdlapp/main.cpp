cdc132718ccf215a2cd8814261ce4dc3a3a9405d
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-07-30 16:20:17 +0200

    Dodaj pliki projektów.

---
+++ sdlapp/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "SDL.h"
+
+int main(int argc,char *argv[]) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("tit", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 300, 300, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(10000);
+
+	return 0;
+}
