38ecc436c2ab418e48a0f7070633867ef9519c58
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-07-31 16:46:53 +0200

    render

--- sdlapp/main.cpp
+++ sdlapp/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include "SDL.h"
+#include "game.h"
+
+game* gamee = nullptr;
 
 int main(int argc,char *argv[]) {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("tit", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 300, 300, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-	SDL_Delay(10000);
+	gamee = new game();
+	gamee->init("test",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800,800, false);
+	while (gamee->running()) {
+		gamee->handleEvents();
+		gamee->update();
+		gamee->render();
 
+	}
+	gamee->clean();
 	return 0;
 }
