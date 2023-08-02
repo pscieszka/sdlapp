09e8b8f92d245229e1eaed683fa374acec5602bb
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-08-02 21:08:04 +0200

    fps

--- sdlapp/main.cpp
+++ sdlapp/main.cpp
@@ -6,13 +6,29 @@ game* gamee = nullptr;
 
 int main(int argc,char *argv[]) {
 
+
+	const int FPS = 10;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
+
 	gamee = new game();
 	gamee->init("test",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800,800, false);
 	while (gamee->running()) {
+
+		frameStart = SDL_GetTicks();
+
 		gamee->handleEvents();
 		gamee->update();
 		gamee->render();
 
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 	gamee->clean();
 	return 0;
